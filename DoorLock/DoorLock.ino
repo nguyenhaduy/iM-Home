f960ef6713262201efb46e03da8fe4066a49818a
Duy Nguyen <nguyenhaduy@tamu.edu>
2017-03-31 13:12:31 -0500

    Update doorlock

--- DoorLock/DoorLock.ino
+++ DoorLock/DoorLock.ino
@@ -1,24 +1,24 @@
 
-#include <Password.h> //http://playground.arduino.cc/uploads/Code/Password.zip //tells to use password library
-#include <Keypad.h> //http://www.arduino.cc/playground/uploads/Code/Keypad.zip  //tells to use keypad library
-#include <Servo.h> //tells to use servo library
+#include <Password.h>
+#include <Keypad.h>
+#include <Servo.h>
 
 
-Servo myservo; //declares servo
-Password password = Password( "0000" ); //password to unlock, can be changed
+Servo myservo;
+Password password = Password( "0000" );
 
 const byte ROWS = 4; // Four rows
 const byte COLS = 4; // columns
 // Define the Keymap
 char keys[ROWS][COLS] = {
-{'1','2','3'},
-{'4','5','6'},
-{'7','8','9'},
-{'*','0','#'}
+{'1','2','3','A'},
+{'4','5','6','B'},
+{'7','8','9','C'},
+{'*','0','#','D'}
 };
-// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte rowPins[ROWS] = { 9, 8, 7, 6 };// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
-byte colPins[COLS] = { 5, 4, 3 };
+
+byte rowPins[ROWS] = { 9, 8, 7, 6 };
+byte colPins[COLS] = { 5, 4, 3, 2 };
 
 
 // Create the Keypad
@@ -39,28 +39,29 @@ void loop(){
   keypad.getKey();
   myservo.write(0);
   }
-  //take care of some special events
-  void keypadEvent(KeypadEvent eKey){
+  
+//take care of some special events
+void keypadEvent(KeypadEvent eKey){
   switch (keypad.getState()){
-  case PRESSED:
-  
-  Serial.print("Enter:");
-  Serial.println(eKey);
-  delay(10);
-  
-  Serial.write(254);
-  
-  switch (eKey){
-    case '*': checkPassword(); delay(1); break;
+    case PRESSED:
     
-    case '#': password.reset(); delay(1); break;
-    
-     default: password.append(eKey); delay(1);
+      Serial.print("Enter:");
+      Serial.println(eKey);
+      delay(10);
+      
+      Serial.write(254);
+      
+      switch (eKey){
+        case '*': checkPassword(); delay(1); break;
+        
+        case '#': password.reset(); delay(1); break;
+        
+         default: password.append(eKey); delay(1);
+    }
+  }
 }
-}
-}
-void checkPassword(){
-  
+
+void checkPassword(){ 
 if (password.evaluate()){  //if password is right open
     
     Serial.println("Accepted");
